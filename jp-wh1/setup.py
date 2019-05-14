1ba8b6dbe22d98efc2261f2690490f6d61aeaea3
mickjigar <jbpatel4@illinois.edu>
2019-05-14 13:23:31 -0500

    added a comment in setup.py

--- jp-wh1/setup.py
+++ jp-wh1/setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+#just adding a comment for GitHub
+
 setup(name='jp_wh1', #use this name i.e. import jp_env1
       version='0.0.1',
       install_requires=['gym'] #and any other dependencies required
